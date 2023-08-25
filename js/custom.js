07890b73c0776c1dbc548a28ca5ff3d73b31cff0
Jaiditya Mathur <jd@Jaidityas-MacBook-Pro.local>
2023-08-25 08:43:38 +0530

    Style changes

--- js/custom.js
+++ js/custom.js
@@ -16,20 +16,9 @@ jQuery(document).ready(function() {
 
   	}) 
 
-	/*
-		Final Countdown Settings
-	*/
-	var finalDate = '2016/11/01';
-
-	$('div.counter').countdown(finalDate)
-   	.on('update.countdown', function(event) {
-
-   		$(this).html(event.strftime('<div class="days-wrapper"><span class="days">%D</span><br>days</div>' + 
-   										 	 '<div class="hours-wrapper"><span class="hours">%H</span><br>hours</div>' + 
-   										 	 '<div class="minutes-wrapper"><span class="minutes">%M</span><br>minutes</div>' +
-   										 	 '<div class="seconds-wrapper"><span class="seconds">%S</span><br>seconds</div>'));
-
-   });  
+	$("#submit").click(function(){
+		$("#email").val("")
+	  });
 
 });
 
